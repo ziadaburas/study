8e0f16e7399585a1d0e64d61f9d513f198f99a33
Ziad Aburas <75611985+ziadaburas@users.noreply.github.com>
2024-11-01 22:05:18 +0300

    Update lect9.js

--- 3A/web/lectures/lect9/lect9.js
+++ 3A/web/lectures/lect9/lect9.js
@@ -1,4 +1,8 @@
-﻿function add(){
+/*
+رابط الصفحة
+https://raw.githack.com/ziadaburas/study/refs/heads/main/3A/web/lectures/lect9/lect9.html
+*/
+function add(){
     let num1 = parseInt(document.getElementById('num1').value)
     let num2 = parseInt(document.getElementById('num2').value)
     document.querySelector('[id=result]').value=num1+num2
