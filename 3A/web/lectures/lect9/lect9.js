bb804a725d958d96a8a4f6b913f1d0cf68245c85
Ziad Aburas <75611985+ziadaburas@users.noreply.github.com>
2024-11-01 20:34:18 +0200

    Add files via upload

---
+++ 3A/web/lectures/lect9/lect9.js
@@ -0,0 +1,6 @@
+﻿function add(){
+    let num1 = parseInt(document.getElementById('num1').value)
+    let num2 = parseInt(document.getElementById('num2').value)
+    document.querySelector('[id=result]').value=num1+num2
+    
+}
